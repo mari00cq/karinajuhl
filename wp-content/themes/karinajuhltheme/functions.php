a059612dea203f3cdd106ff9b8fbcd0c8b00f5a8
Maria Sattrup <maria.sattrup@gmail.com>
2022-05-23 12:35:44 +0200

    childtheme

---
+++ wp-content/themes/karinajuhltheme/functions.php
@@ -0,0 +1,7 @@
+<?php
+add_action( 'wp_enqueue_scripts', 'enqueue_important_files' );
+function enqueue_important_files() {
+/*hent parent stylesheet i parenttemaets mappe*/
+wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+}
+?>
