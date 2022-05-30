ca37dac41a0708450dece61a32e1cfd100a4f7d8
livx5199 <livx5199@stud.kea.dk>
2022-05-30 12:58:45 +0200

    footer

--- wp-content/themes/karinajuhltheme/template-parts/footer/site-info.php
+++ wp-content/themes/karinajuhltheme/template-parts/footer/site-info.php
@@ -9,6 +9,49 @@
  */
 
 ?>
+<style>
+
+	.copyright img {
+		max-width: 60%;
+	}
+
+	.site-info .copyright {
+		display: grid;
+		grid-template-columns: 1fr 1fr 1fr;
+	}
+
+	.copyright > span:nth-child(1){
+		grid-column: 1/2;
+		place-self: center;
+	}
+
+	.copyright > span:nth-child(2){
+		grid-column: 2/3;
+		place-self: center;
+	}
+
+	.site-info .copyright span:nth-child(2){
+		text-align: center;	
+	}
+
+	.copyright > span:nth-child(3){
+		grid-column: 3/4;
+		place-self: center;	
+	}
+
+	.copyright p {
+		text-align: center;
+		color: black;
+	}
+
+	@media screen and (max-width:480px) {
+		.site-info {
+		font-size: 0.5rem;
+		}
+	}
+
+</style>
+
 <div class="site-info">
 	<?php
 	if ( function_exists( 'the_privacy_policy_link' ) ) {
@@ -17,7 +60,10 @@
 	?>
 	<span class="copyright">
 		<span>
-		<p>Vores Info</p>
+		<p>ADRESSE</p>
+		<p>Hyben Alle 92</p>
+		<p>2770 Kastrup</p>
+		<p>CVR: 29357021</p>
 		</span>
 
         <span>
@@ -26,16 +72,20 @@
         </span>
 		
 		<span>
-			<p>Info</p>
+			<p>KONTAKT</p>
+			<p>Tlf: 50 80 11 50</p>
+			<p>Mail: yelvabeauty@gmail.com</p>
 </span>
 	
 	</span>
 </div><!-- .site-info -->
 <style>
       #colophon {
-  background-image: url("http://www.mariasattrup.dk/kea/karinajuhl/wp-content/uploads/2022/05/footer_traer.jpg");
+  background-image: url("http://www.mariasattrup.dk/kea/karinajuhl/wp-content/uploads/2022/05/Image-2.jpg");
   background-repeat: no-repeat;
-    background-size: 140%;
+    background-size: cover;
+
+
 
 }
 
